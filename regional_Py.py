4414cc1cbbb7b163ea81f8e4c251c00e167b965e
Mariah E. Weavil-Abueg <mariah93@a001.grid.gs.washington.edu>
2016-02-09 11:56:19 -0800

    changed header to tabix_List

--- regional_Py.py
+++ regional_Py.py
@@ -15,11 +15,11 @@ def open_tabix (chr_Num):
                 myTabix = pysam.TabixFile('/net/akey/vol1/scratch/1KGenomes_VCF/ALL.%s.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.bgz' %(chr_Pos))
                 header = myTabix.header
                 tabix_List.append(myTabix)
-#	return tabix_List
-	return header
+	return tabix_List
+#	return header
 
-def VCF (header):
-        for line in header:
+def VCF (tabix_List):
+        for line in tabix_List:
                 #Skips line that aren't needed
                 if line.startswith('##'):
                         continue
@@ -168,7 +168,7 @@ regions.readline()
 tabix_List = open_tabix(chr_Num)
 
 # Puts all individuals from VCF and tabix files into lists
-inds = VCF(header)
+inds = VCF(tabix_List)
 
 #Finds position of common individuals
 all_Inds = common_inds(inds, selectInds)
